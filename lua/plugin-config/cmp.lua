0e4dd566a57cf61792f31f3698f92e9fa0d48fd4
zhangyang04_dmx <zhangyang04@duxiaoman.com>
2022-04-28 19:21:48 +0800

    conf

--- lua/plugin-config/cmp.lua
+++ lua/plugin-config/cmp.lua
@@ -119,11 +119,11 @@ cmp.setup({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	window({
+	window = {
 		documentation = {
 			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
 		},
-	}),
+	},
 	experimental = {
 		ghost_text = false,
 		native_menu = false,
